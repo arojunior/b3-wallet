0209134a2c9b246ab9860799a4f3ea0b4352be32
Junior Oliveira <arojunior@gmail.com>
2020-05-01 14:31:43 +0200

    eslint fixes

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,50 +1,45 @@
-exports.DATA_FOLDER =  __dirname + '/../data';
+exports.DATA_FOLDER = `${__dirname}/../data`;
 
 exports.FILES = {
-  EXTRACT: 'b3_result.json',
-  STOCKS: 'stocks.json',
-  STOCKS_TOTAL: 'acc-stocks.json',
-  OPTIONS: 'stocks-options.json',
-  OPTIONS_TOTAL: 'acc-stocks-options.json',
-  FULL: 'full-operations.json',
-  WALLET: 'wallet.json',
-  CREDENTIALS: 'credentials.json',
+  EXTRACT: `b3_result.json`,
+  STOCKS: `stocks.json`,
+  STOCKS_TOTAL: `acc-stocks.json`,
+  OPTIONS: `stocks-options.json`,
+  OPTIONS_TOTAL: `acc-stocks-options.json`,
+  FULL: `full-operations.json`,
+  WALLET: `wallet.json`,
+  CREDENTIALS: `credentials.json`,
 };
 
 exports.MONTHS = {
-  1: "Janeiro",
-  2: "Fevereiro",
-  3: "Março",
-  4: "Abril",
-  5: "Maio",
-  6: "Junho",
-  7: "Julho",
-  8: "Agosto",
-  9: "Setembro",
-  10: "Outubro",
-  11: "Novembro",
-  12: "Dezembro"
+  1: `Janeiro`,
+  2: `Fevereiro`,
+  3: `Março`,
+  4: `Abril`,
+  5: `Maio`,
+  6: `Junho`,
+  7: `Julho`,
+  8: `Agosto`,
+  9: `Setembro`,
+  10: `Outubro`,
+  11: `Novembro`,
+  12: `Dezembro`,
 };
 
 exports.COLUMNS = {
-  PRICE: "Preco_(R$)",
-  TOTAL_PRICE: "Valor_Total(R$)",
-  DATE: "Data_do_Negocio",
-  MONTH: "Mes",
-  OPERATION: "Compra/Venda",
-  TICKER: "Codigo_Negociacao",
-  QUANTITY: "Quantidade",
+  PRICE: `Preco_(R$)`,
+  TOTAL_PRICE: `Valor_Total(R$)`,
+  DATE: `Data_do_Negocio`,
+  MONTH: `Mes`,
+  OPERATION: `Compra/Venda`,
+  TICKER: `Codigo_Negociacao`,
+  QUANTITY: `Quantidade`,
 };
 
-exports.OPCOES = ["Opção de Compra", "Opção de Venda"];
+exports.OPCOES = [`Opção de Compra`, `Opção de Venda`];
 
-exports.ACOES = ["Merc. Fracionário", "Mercado a Vista"];
+exports.ACOES = [`Merc. Fracionário`, `Mercado a Vista`];
 
-exports.OLD_TICKERS = [
-  'NATU3',
-  'NATU3F',
-  'BMGB11',
-  'BMGB11F'
-];
+exports.OLD_TICKERS = [`NATU3`, `NATU3F`, `BMGB11`, `BMGB11F`];
 
 exports.PORT = 9990;
